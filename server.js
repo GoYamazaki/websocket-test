66a383933e79e4a8d8c35d59fafd3ded282d3811
Go Yamazaki <go.yamazaki@woven-planet.global>
2021-06-30 14:40:24 +0900

    Add view for the OAuth Redirection

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
+  if(res.query?.hasOwnProperty('code')){
+    console.log(`OK Your code is ${res.query.code}`)
+    res.render('ok', { code: res.query.code });
+    return;
+  }
   res.sendFile('index.html', {root: __dirname});
 });
 
