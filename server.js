63b48c2afab250d6b1c1757b1695f7a010541283
Go Yamazaki <go.yamazaki@woven-planet.global>
2021-06-30 12:04:15 +0900

    add zoom auth link

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
 'use strict';
 
-const express = require('express');
-const { Server } = require('ws');
+var express = require('express');
+var app = express();
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const wss = new Server({ server });
-
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+app.get('/', function(req, res){
+  res.sendFile('index.html', {root: __dirname});
 });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-  	var now  = new Date();
-  	var hour = `${now.getHours()}`.padStart(2, '0');
-  	var min  = `${now.getMinutes()}`.padStart(2, '0');
-  	var sec  = `${now.getSeconds()}`.padStart(2, '0');
-    client.send(`${hour}:${min}:${sec}`);
-  });
-}, 1000);
+var server = app.listen(PORT, ()=>{
+                  console.log(`Listening on ${PORT}`);
+                });
